146cfcee409ef5c84ee20e2dbf78ddabec37cb84
BerZeNot <xxpauloxx8@gmail.com>
2022-10-31 22:32:53 -0300

    chore: initial login and project struture

---
+++ view/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/login.css">
+    <title>Login</title>
+</head>
+<body>
+    <main>
+        <div class="login-wrapper">
+            <div>
+                <label for="login">Login: </label>
+                <input type="text" name="login" id="login">
+            </div>
+            <div>
+                <label for="password">Password: </label>
+                <input type="password">
+            </div>
+            <div>
+                <input type="submit" value="Login">
+                <input type="button" value="Sign up">
+            </div>
+        </div>
+    </main>
+</body>
+</html>
